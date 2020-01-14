fbe0b25bdcc7a78815db7801b08a10d6f5acc404
Drake Wilcox and Joseph Wangemann <drake@>
2020-01-13 16:33:13 -0800

    add three functions to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,32 @@ var lastLetter = sentence.charAt(sentence.length-1);
 
 console.log(firstLetter, lastLetter);
 
+
+//function for taking first & last letter and capitalizing them
 var firstLast;
 function capitalize(firstLetter, lastLetter) {
   return firstLast = (firstLetter + lastLetter).toUpperCase();
 };
 
-capitalize(firstLetter, lastLetter);
 
+//function for taking first & last letter and reversing them
+var letterReverse;
+function reverse(firstLast) {
+  return letterReverse = firstLast.split("").reverse().join("").toLowerCase();
+};
+
+//function for taking original sentence and squishes reversed letters at end.
+var nextVar;
+function squish(sentence, letterReverse) {
+  nextVar = sentence + letterReverse;
+}
+
+capitalize(firstLetter, lastLetter);
+reverse(firstLast);
+squish(sentence, letterReverse.toUpperCase());
 console.log(firstLast);
+console.log(letterReverse);
 
 alert(firstLast);
+alert(letterReverse);
+alert(nextVar);
